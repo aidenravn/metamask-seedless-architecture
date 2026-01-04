35dee46990f025f0affddf9f7d3ef555e26d2907
ravN <tayfunmalatyali@gmail.com>
2026-01-04 03:09:09 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,24 @@
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle"); // Testler için chai + waffle desteği
 
 module.exports = {
   solidity: "0.8.20",
+  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
-    localhost: { url: "http://127.0.0.1:8545" },
+    hardhat: {
+      chainId: 31337, // Hardhat varsayılan lokal chain id
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    // Testnet eklemek istersen örnek:
+    // goerli: {
+    //   url: "https://goerli.infura.io/v3/YOUR_INFURA_KEY",
+    //   accounts: ["PRIVATE_KEY"]
+    // }
+  },
+  mocha: {
+    timeout: 20000, // Testlerin yeterli süre alması için
   },
 };
