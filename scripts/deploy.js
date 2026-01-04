b73514e80932120b8ea1c5881132f44a20d1ab38
ravN <tayfunmalatyali@gmail.com>
2026-01-04 03:07:39 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,15 @@
 const hre = require("hardhat");
 
 async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with account:", deployer.address);
+
+  // IdentityRegistry deploy
   const IdentityRegistry = await hre.ethers.getContractFactory("IdentityRegistry");
   const registry = await IdentityRegistry.deploy();
   await registry.deployed();
+
   console.log("IdentityRegistry deployed to:", registry.address);
-
-  const Reputation = await hre.ethers.getContractFactory("ReputationContract");
-  const reputation = await Reputation.deploy(registry.address);
-  await reputation.deployed();
-  console.log("ReputationContract deployed to:", reputation.address);
-
-  const Migration = await hre.ethers.getContractFactory("MigrationHelper");
-  const migration = await Migration.deploy();
-  await migration.deployed();
-  console.log("MigrationHelper deployed to:", migration.address);
 }
 
 main().catch((error) => {
